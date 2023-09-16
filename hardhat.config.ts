0158c5e1c61fb9ee5d7924dca610755ecf732aa3
wtsuccess <white991120@gmail.com>
2023-09-16 14:53:20 -0400

    update deploy

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -15,38 +15,38 @@ console.log(POLYGONSCAN_KEY);
 
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
-task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
-  const accounts = await hre.ethers.getSigners();
+// task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
+//   const accounts = await hre.ethers.getSigners();
 
-  for (const account of accounts) {
-    console.log(account.address);
-  }
-});
+//   for (const account of accounts) {
+//     console.log(account.address);
+//   }
+// });
 
-function node(networkName: string) {
-  const fallback = 'http://localhost:8545';
-  const uppercase = networkName.toUpperCase();
-  const uri = process.env[`NODE_${uppercase}`] || fallback;
-  return uri.replace('{{NETWORK}}', networkName);
-}
+// function node(networkName: string) {
+//   const fallback = 'http://localhost:8545';
+//   const uppercase = networkName.toUpperCase();
+//   const uri = process.env[`NODE_${uppercase}`] || fallback;
+//   return uri.replace('{{NETWORK}}', networkName);
+// }
 
-function accounts(networkName: string) {
-  const uppercase = networkName.toUpperCase();
-  const accounts = process.env[`ACCOUNTS_${uppercase}`] || '';
-  return accounts
-    .split(',')
-    .map((account) => account.trim())
-    .filter(Boolean);
-}
+// function accounts(networkName: string) {
+//   const uppercase = networkName.toUpperCase();
+//   const accounts = process.env[`ACCOUNTS_${uppercase}`] || '';
+//   return accounts
+//     .split(',')
+//     .map((account) => account.trim())
+//     .filter(Boolean);
+// }
 
 // You need to export an object to set up your config
 // Go to https://hardhat.org/config/ to learn more
-interface CustomUserConfig extends HardhatUserConfig {
-  namedAccounts: any
-}
+// interface CustomUserConfig extends HardhatUserConfig {
+//   namedAccounts: any
+// }
 
 
-const config: CustomUserConfig = {
+const config: HardhatUserConfig = {
   solidity: {
     compilers: [
       {
@@ -78,30 +78,30 @@ const config: CustomUserConfig = {
       },
     ]
   },
-  namedAccounts: {
-    deployer: 0,
-    minter: 1,
-    owner0: 5,
-    owner1: 2,
-    owner2: 3,
-    owner3: 4
-  },
+  // namedAccounts: {
+  //   deployer: 0,
+  //   minter: 1,
+  //   owner0: 5,
+  //   owner1: 2,
+  //   owner2: 3,
+  //   owner3: 4
+  // },
   defaultNetwork: "hardhat",
 
   networks: {
-    mainnet: {
-      hardfork: 'istanbul',
-      url: node('mainnet'),
-      accounts: accounts('mainnet')
-    },
-    ropsten: {
-      url: node("ropsten"),
-      accounts: accounts('ropsten')
-    },
-    rinkeby: {
-      url: node("rinkeby"),
-      accounts: accounts('rinkeby')
-    },
+    // mainnet: {
+    //   hardfork: 'istanbul',
+    //   url: node('mainnet'),
+    //   accounts: accounts('mainnet')
+    // },
+    // ropsten: {
+    //   url: node("ropsten"),
+    //   accounts: accounts('ropsten')
+    // },
+    // rinkeby: {
+    //   url: node("rinkeby"),
+    //   accounts: accounts('rinkeby')
+    // },
     polygon: {
       url: "https://polygon-bor.publicnode.com",
       accounts: [`0x${PRIVATE_KEY}`],
@@ -111,12 +111,14 @@ const config: CustomUserConfig = {
       accounts: [`0x${PRIVATE_KEY}`],
     },
   },
-  gasReporter: {
-    enabled: process.env.REPORT_GAS !== undefined,
-    currency: "USD",
-  },
+  // gasReporter: {
+  //   enabled: process.env.REPORT_GAS !== undefined,
+  //   currency: "USD",
+  // },
   etherscan: {
-    apiKey: POLYGONSCAN_KEY,
+    apiKey: {
+      polygonMumbai: POLYGONSCAN_KEY
+    },
   },
 };
 
