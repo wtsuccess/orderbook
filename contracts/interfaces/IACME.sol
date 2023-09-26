af9240a48291cb8810bb31f1c34061349a3e325e
wtsuccess <white991120@gmail.com>
2023-09-26 18:30:52 -0400

    view

--- contracts/interfaces/IACME.sol
+++ contracts/interfaces/IACME.sol
@@ -3,7 +3,7 @@
 pragma solidity 0.8.18;
 
 interface IACME {
-    function isBlacklisted(address _user) external returns (bool);
+    function isBlacklisted(address _user) external view returns (bool);
     function setBlacklist(address _user) external; 
     function setWhitelist(address _user) external;
 }
